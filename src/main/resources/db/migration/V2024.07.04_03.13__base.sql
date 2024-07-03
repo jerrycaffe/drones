cc379ff8360508151a1a369a332f5a27a626d110
jerrycaffe <jerrycaffe@gmail.com>
2024-07-03 05:33:09 +0100

    added initial migration for the table

--- src/main/resources/db/migration/V2024.07.04_03.13__base.sql
+++ src/main/resources/db/migration/V2024.07.04_03.13__base.sql
@@ -1,18 +1,20 @@
-CREATE TABLE IF NOT EXISTS drones(
- serial_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- model NOT NULL VARCHAR,
- weight_limit DECIMAL NOT NULL,
- battery_capacity INT NOT NULL,
- state NOT NULL VARCHAR
-)
+CREATE TABLE IF NOT EXISTS drones (
+    serial_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    model VARCHAR(50) NOT NULL,
+    weight_limit DECIMAL(10, 2) NOT NULL,
+    battery_capacity INT NOT NULL,
+    state VARCHAR(30) NOT NULL
+);
+
 
 CREATE TABLE IF NOT EXISTS medication(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name NOT NULL VARCHAR,
-weight NOT NULL DECIMAL,
-code VARCHAR NOT NULL,
+name VARCHAR(100) NOT NULL,
+weight DECIMAL(10, 2) NOT NULL,
+code VARCHAR(100) NOT NULL,
 image VARCHAR,
 drone_serial_number INT NOT NULL,
 CONSTRAINT fk_drones_medication_drone_serial_number FOREIGN KEY (drone_serial_number) REFERENCES drones(serial_number) ON DELETE CASCADE
 )
+
 
