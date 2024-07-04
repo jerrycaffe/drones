cf207a734c582f10b2a6412e238372e206de1f67
jerrycaffe <jerrycaffe@gmail.com>
2024-07-04 04:23:15 +0100

    model for medication

--- src/main/java/com/musalasoft/drones/drones/model/Medication.java
+++ src/main/java/com/musalasoft/drones/drones/model/Medication.java
@@ -1,4 +1,27 @@
 package com.musalasoft.drones.drones.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "medications")
 public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal weight;
+    private String code;
+
+    @Lob
+    private byte[] image;
+
+    @ManyToOne
+    @JoinColumn(name = "drone_id")
+    private Drone drone;
 }
