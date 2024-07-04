e7ad83bcdefcaf02fdebf863244578de7ad8f698
jerrycaffe <jerrycaffe@gmail.com>
2024-07-04 04:22:17 +0100

    create repo for medication

--- src/main/java/com/musalasoft/drones/drones/repository/MedicationRepository.java
+++ src/main/java/com/musalasoft/drones/drones/repository/MedicationRepository.java
@@ -1,4 +1,7 @@
 package com.musalasoft.drones.drones.repository;
 
-public interface MedicationRepository {
+import com.musalasoft.drones.drones.model.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
 }
