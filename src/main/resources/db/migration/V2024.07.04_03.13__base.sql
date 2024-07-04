4e23c7ee31b3899c9e40c6901fe5a03cfdb618bc
jerrycaffe <jerrycaffe@gmail.com>
2024-07-04 04:20:47 +0100

    modified migration file for model

--- src/main/resources/db/migration/V2024.07.04_03.13__base.sql
+++ src/main/resources/db/migration/V2024.07.04_03.13__base.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS drones (
-    serial_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    serial_number VARCHAR(100) NOT NULL,
     model VARCHAR(50) NOT NULL,
     weight_limit DECIMAL(10, 2) NOT NULL,
     battery_capacity INT NOT NULL,
@@ -7,14 +8,14 @@ CREATE TABLE IF NOT EXISTS drones (
 );
 
 
-CREATE TABLE IF NOT EXISTS medication(
+CREATE TABLE IF NOT EXISTS medications(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
 weight DECIMAL(10, 2) NOT NULL,
 code VARCHAR(100) NOT NULL,
-image VARCHAR,
-drone_serial_number INT NOT NULL,
-CONSTRAINT fk_drones_medication_drone_serial_number FOREIGN KEY (drone_serial_number) REFERENCES drones(serial_number) ON DELETE CASCADE
+image BLOB,
+drone_id INT NOT NULL,
+CONSTRAINT fk_drones_medications_drone_id FOREIGN KEY (drone_id) REFERENCES drones(id) ON DELETE CASCADE
 )
 
 
