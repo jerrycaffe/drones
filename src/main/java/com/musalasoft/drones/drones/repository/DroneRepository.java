6d2ebe3e2abdfc9aecd823f0b5c63380aaa6be20
jerrycaffe <jerrycaffe@gmail.com>
2024-07-06 10:11:54 +0100

    added repository to check if drone is available

--- src/main/java/com/musalasoft/drones/drones/repository/DroneRepository.java
+++ src/main/java/com/musalasoft/drones/drones/repository/DroneRepository.java
@@ -1,7 +1,11 @@
 package com.musalasoft.drones.drones.repository;
 
 import com.musalasoft.drones.drones.model.Drone;
+import com.musalasoft.drones.drones.model.DroneStateEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DroneRepository extends JpaRepository<Drone, Long> {
+    List<Drone> findAllByBatteryCapacityGreaterThanAndState(int batteryLevel, DroneStateEnum stateEnum);
 }
