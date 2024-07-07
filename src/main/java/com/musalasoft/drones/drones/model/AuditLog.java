815e7ec699363c54e6cf84bff1b7360761e91d3f
jerrycaffe <jerrycaffe@gmail.com>
2024-07-07 10:19:53 +0100

    added audit model

--- src/main/java/com/musalasoft/drones/drones/model/AuditLog.java
+++ src/main/java/com/musalasoft/drones/drones/model/AuditLog.java
@@ -1,2 +1,21 @@
-package com.musalasoft.drones.drones.model;public class AuditLog {
+package com.musalasoft.drones.drones.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "audit_log")
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long droneId;
+    private int batteryCapacity;
+    private LocalDateTime timestamp;
 }
