00c7cc14753a0bd891921e7c6f17d74c0b50a1c1
jerrycaffe <jerrycaffe@gmail.com>
2024-07-07 22:15:54 +0100

    added image url

--- src/main/resources/db/migration/V2024.07.05_18.27__seeding.sql
+++ src/main/resources/db/migration/V2024.07.05_18.27__seeding.sql
@@ -5,6 +5,6 @@ VALUES('J234', 0, 20.60, 100,0, NOW()),
 ('J236', 3, 500.30, 20,3, NOW());
 
 INSERT INTO medication(name, weight, code, image, drone_id, created_at)
-VALUES('test1', 5.20, 'testing', '', 2, NOW()),
-('test2', 5.00, 'tested', '', 2, NOW()),
-('test3', 4.70, 'trying', '', 3, NOW())
+VALUES('test1', 5.20, 'testing', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwqRVCWi62Zq8bY5m4L58DEGbwYK9zNuBUgw&s', 2, NOW()),
+('test2', 5.00, 'tested', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwqRVCWi62Zq8bY5m4L58DEGbwYK9zNuBUgw&s', 2, NOW()),
+('test3', 4.70, 'trying', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwqRVCWi62Zq8bY5m4L58DEGbwYK9zNuBUgw&s', 3, NOW())
