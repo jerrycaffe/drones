0883aeae80e05116f1119adf829932b4766eaeb8
jerrycaffe <jerrycaffe@gmail.com>
2024-07-07 10:21:57 +0100

    added scheduler service

--- src/main/java/com/musalasoft/drones/drones/service/ScheduleService.java
+++ src/main/java/com/musalasoft/drones/drones/service/ScheduleService.java
@@ -1,2 +1,15 @@
-package com.musalasoft.drones.drones.service;public class ScheduleService {
+package com.musalasoft.drones.drones.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final DroneService droneService;
+    @Scheduled(cron = "0 */3 * * * ?")  // Every three minute
+    public void checkDroneBatteries() {
+        droneService.checkAllBatteryLevels();
+    }
 }
