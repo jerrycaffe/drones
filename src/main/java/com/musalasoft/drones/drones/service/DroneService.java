12cf13311c23946bd195b18bdac46679394e472e
jerrycaffe <jerrycaffe@gmail.com>
2024-07-07 10:21:15 +0100

    this is an update

--- src/main/java/com/musalasoft/drones/drones/service/DroneService.java
+++ src/main/java/com/musalasoft/drones/drones/service/DroneService.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface DroneService {
     Drone register(DroneRegisterReq droneRegisterRequest);
     Medication load(DroneLoadReq droneLoadReq, Long droneId);
-//    DroneCheckResp check(Long droneId);
+
     void checkAllBatteryLevels();
     BatteryDetails checkBattery(Long id);
     List<Drone> availableDrones();
