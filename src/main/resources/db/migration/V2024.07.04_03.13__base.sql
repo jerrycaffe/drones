83a4cdc18014f4127262ba3aeaaa51574829dbba
jerrycaffe <jerrycaffe@gmail.com>
2024-07-07 10:22:20 +0100

    added audit log to the migration

--- src/main/resources/db/migration/V2024.07.04_03.13__base.sql
+++ src/main/resources/db/migration/V2024.07.04_03.13__base.sql
@@ -4,7 +4,9 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     model VARCHAR(50) NOT NULL,
     weight_limit DECIMAL(10, 2) NOT NULL,
     battery_capacity INT NOT NULL,
-    state VARCHAR(30) NOT NULL
+    state VARCHAR(30) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+
 );
 
 
@@ -15,7 +17,14 @@ weight DECIMAL(10, 2) NOT NULL,
 code VARCHAR(100) NOT NULL,
 image VARCHAR,
 drone_id INT NOT NULL,
+created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 CONSTRAINT fk_drones_medications_drone_id FOREIGN KEY (drone_id) REFERENCES drone(id) ON DELETE CASCADE
-)
+);
 
+CREATE TABLE IF NOT EXISTS audit_log(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+battery_capacity INT NOT NULL,
+drone_id INT NOT NULL,
+timestamp TIMESTAMP NOT NULL DEFAULT NOW()
+)
 
